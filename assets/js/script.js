d99264e64f269ff7890c7c77c03d835073717aa2
Milleny Ellen Jodas Ferreira <ellenmilleny@gmail.com>
2024-11-28 19:09:50 -0300

    style: responsive header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 window.addEventListener("scroll", () => {
     const header = document.querySelector(".header");
     header.classList.toggle("scroll", window.scrollY > 0);
-  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const bar = document.querySelector(".menu");
+    const menu = document.querySelector(".header_menu");
+
+    bar.addEventListener("click", () => {
+        menu.classList.toggle("show_menu");
+    });
+});
