fdd637c3e6616a731efd274c2f016604d5f6df43
Milleny Ellen Jodas Ferreira <ellenmilleny@gmail.com>
2024-11-28 23:17:03 -0300

    feat: add skills section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", () => {
-    const header = document.querySelector(".header");
+    const header = document.querySelector(".header_nav");
     header.classList.toggle("scroll", window.scrollY > 0);
 });
 
@@ -11,3 +11,53 @@ document.addEventListener("DOMContentLoaded", function () {
         menu.classList.toggle("show_menu");
     });
 });
+
+const swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    grid: {
+      fill: "row",
+      rows: 1,
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    a11y: {
+      prevSlideMessage: "Previous slide",
+      nextSlideMessage: "Next slide",
+    },
+    breakpoints: {
+        500: {
+            slidesPerView: 4,
+            grid: {
+              fill: "row",
+              rows: 1,
+            },
+          },
+        900: {
+        slidesPerView: 6,
+        grid: {
+          fill: "row",
+          rows: 1,
+        },
+      },
+      1024: {
+        slidesPerView: 10,
+        grid: {
+          fill: "row",
+          rows: 1,
+        },
+      },
+    },
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },
+    loop: true,
+  });
